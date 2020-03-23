f80611ed9e205addb634c482a5199c684aa8a5ae
Shuzo Tanaka <54611804+SHUZO-TANAKA@users.noreply.github.com>
2020-03-23 11:45:02 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+function main(input) {
+    console.log(input);
+}
+main(require('fs').readFileSync('/dev/stdin', 'utf-8'));
+//# sourceMappingURL=test.js.map
---
+++ test.ts
@@ -0,0 +1,10 @@
+function main(input){
+    console.log(input);
+}
+
+
+
+
+
+
+main(require('fs').readFileSync('/dev/stdin','utf-8'));
